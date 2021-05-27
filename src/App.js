f342073d437e22be468ab93c9657312f8631ee32
KKH-pixel <kendra.hunt@seattlecentral.edu>
2021-05-27 10:33:09 -0700

    updated front end pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import PlantCard from "./components/PlantCard";
+import Home from "./components/pages/Home";
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+  };
+  render = () => {
+    return (
+      <div>
+        <NavBar></NavBar>
+        <Home></Home>
+      </div>
+    );
+  };
 }
 
 export default App;
