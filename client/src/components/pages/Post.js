da3ac8cdaceaa64d461567e8cc76be78a9b6158a
KKH-pixel <kendra.hunt@seattlecentral.edu>
2021-06-01 10:02:22 -0700

    working post page minus image upload

--- client/src/components/pages/Post.js
+++ client/src/components/pages/Post.js
@@ -1,23 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 // // import './index.css'
 
 // import ImageUpload from "../ImageUpload";
 
-import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
-import {Cloudinary} from 'cloudinary-core';
+// import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+// import {Cloudinary} from 'cloudinary-core';
+
 import { makeStyles } from '@material-ui/core/styles';
-import FilledInput from '@material-ui/core/FilledInput';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormGroup from '@material-ui/core/FormGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
+// import FormHelperText from '@material-ui/core/FormHelperText';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Checkbox from '@material-ui/core/Checkbox';
 
-import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Grid from '@material-ui/core/Grid';
@@ -31,13 +30,49 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ComposedTextField() {
-  const [name, setName] = React.useState('Composed TextField');
-  const [value, setValue] = React.useState('default');
+  const [name, setName] = React.useState({
+      plant: "",
+      instruct: ""
+    });
+  
+    const { plant, instruct } = name;
+
+    const handleChange = (event) => {
+        setName({ ...name, [event.target.name]: event.target.value });
+      };
+
+  
+    const [value, setValue] = React.useState({
+      flowers: "",
+      water: "",
+      sunlight: "",
+      fertilizer: "",
+      temperature: ""
+  });
+
+  const { flowers, water, sunlight, fertilizer, temperature } = value;
+
+  const handleValueChange = (event) => {
+    setValue({ ...value, [event.target.name]: event.target.value });
+  };
+
+
+  const [checkState, setCheckState] = React.useState({
+    pets: false,
+    easycare: false,
+    exotic: false,
+    restricted: false,
+  });
+
+  const { pets, easycare, exotic, restricted } = checkState;
+
+  const handleCheckChange = (event) => {
+    setCheckState({ ...checkState, [event.target.name]: event.target.checked });
+};
+
+
   const classes = useStyles();
 
-  const handleChange = (event) => {
-    setName(event.target.value);
-  };
 
   return (
     <div className={classes.root}>
@@ -51,7 +86,7 @@ export default function ComposedTextField() {
         <Grid item xs={12}>
             <FormControl variant="outlined">
                 <InputLabel htmlFor="component-outlined">Plant Name</InputLabel>
-                <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Name" />
+                <OutlinedInput id="component-outlined" value={plant} onChange={handleChange} label="Name" name="plant" />
             </FormControl>
         </Grid>
         <Grid item xs={12}>
@@ -59,19 +94,19 @@ export default function ComposedTextField() {
                 <FormLabel component="legend">Pick all that apply</FormLabel>
                     <FormGroup>
                         <FormControlLabel
-                            control={<Checkbox onChange={handleChange} name="pets" />}
+                            control={<Checkbox checked={pets} onChange={handleCheckChange} name="pets" />}
                             label="Pet Friendly"
                         />
                         <FormControlLabel
-                            control={<Checkbox onChange={handleChange} name="easycare" />}
+                            control={<Checkbox checked={easycare} onChange={handleCheckChange} name="easycare" />}
                             label="Low Maintenance"
                         />
                         <FormControlLabel
-                            control={<Checkbox onChange={handleChange} name="exotic" />}
+                            control={<Checkbox checked={exotic} onChange={handleCheckChange} name="exotic" />}
                             label="Exotic"
                         />
                         <FormControlLabel
-                            control={<Checkbox onChange={handleChange} name="restricted" />}
+                            control={<Checkbox checked={restricted} onChange={handleCheckChange} name="restricted" />}
                             label="Shipping Restrictions May Apply"
                         />
                     </FormGroup>
@@ -80,56 +115,56 @@ export default function ComposedTextField() {
         <Grid item xs={12}>
         <FormControl component="fieldset">
             <FormLabel component="legend">Flowers</FormLabel>
-                <RadioGroup aria-label="flowers" name="flowers1" value={value} onChange={handleChange}>
-                    <FormControlLabel control={<Radio />} label="Yes" />
-                    <FormControlLabel value="default" control={<Radio />} label="No" />
+                <RadioGroup aria-label="flowers" name="flowers" value={flowers} onChange={handleValueChange}>
+                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                    <FormControlLabel value="no" control={<Radio />} label="No" />
                 </RadioGroup>
             </FormControl>
         </Grid>
         <Grid item xs={12}>
         <FormControl component="fieldset">
             <FormLabel component="legend">Water</FormLabel>
-                <RadioGroup aria-label="water" name="water1" value={value} onChange={handleChange}>
-                    <FormControlLabel control={<Radio />} label="Daily" />
-                    <FormControlLabel value="default" control={<Radio />} label="1-2 times a week" />
-                    <FormControlLabel control={<Radio />} label="Once a month" />
-                    <FormControlLabel control={<Radio />} label="Air plant" />
+                <RadioGroup aria-label="water" name="water" value={water} onChange={handleValueChange}>
+                    <FormControlLabel value="daily" control={<Radio />} label="Daily" />
+                    <FormControlLabel value="weekly" control={<Radio />} label="1-2 times a week" />
+                    <FormControlLabel value="monthly" control={<Radio />} label="Once a month" />
+                    <FormControlLabel value="none" control={<Radio />} label="Air plant" />
                 </RadioGroup>
             </FormControl>
         </Grid>
         <Grid item xs={12}>
         <FormControl component="fieldset">
             <FormLabel component="legend">Sunlight</FormLabel>
-                <RadioGroup aria-label="sunlight" name="sunlight1" value={value} onChange={handleChange}>
-                    <FormControlLabel value="default" control={<Radio />} label="Direct Sunlight" />
-                    <FormControlLabel control={<Radio />} label="Indirect Sunlight" />
+                <RadioGroup aria-label="sunlight" name="sunlight" value={sunlight} onChange={handleValueChange}>
+                    <FormControlLabel value="direct" control={<Radio />} label="Direct Sunlight" />
+                    <FormControlLabel value="indirect" control={<Radio />} label="Indirect Sunlight" />
                 </RadioGroup>
             </FormControl>
         </Grid>
         <Grid item xs={12}>
         <FormControl component="fieldset">
-            <FormLabel component="legend">Sunlight</FormLabel>
-                <RadioGroup aria-label="ferilize" name="fertilize1" value={value} onChange={handleChange}>
-                    <FormControlLabel control={<Radio />} label="Regularly" />
-                    <FormControlLabel value="default" control={<Radio />} label="Periodically" />
-                    <FormControlLabel control={<Radio />} label="Rarely" />
+            <FormLabel component="legend">Ferilizer</FormLabel>
+                <RadioGroup aria-label="ferilizer" name="fertilizer" value={fertilizer} onChange={handleValueChange}>
+                    <FormControlLabel value="regularly" control={<Radio />} label="Regularly" />
+                    <FormControlLabel value="periodically" control={<Radio />} label="Periodically" />
+                    <FormControlLabel value="rarely" control={<Radio />} label="Rarely" />
                 </RadioGroup>
             </FormControl>
         </Grid>
         <Grid item xs={12}>
         <FormControl component="fieldset">
-            <FormLabel component="legend">Sunlight</FormLabel>
-                <RadioGroup aria-label="temperature" name="temperature1" value={value} onChange={handleChange}>
-                    <FormControlLabel control={<Radio />} label="Hot" />
-                    <FormControlLabel value="default" control={<Radio />} label="Temperate" />
-                    <FormControlLabel control={<Radio />} label="Cool" />
+            <FormLabel component="legend">Temperature</FormLabel>
+                <RadioGroup aria-label="temperature" name="temperature" value={temperature} onChange={handleValueChange}>
+                    <FormControlLabel value="hot" control={<Radio />} label="Hot" />
+                    <FormControlLabel value="temperate" control={<Radio />} label="Temperate" />
+                    <FormControlLabel value="cool" control={<Radio />} label="Cool" />
                 </RadioGroup>
             </FormControl>
         </Grid>
         <Grid item xs={12}>
             <FormControl variant="outlined">
                 <InputLabel htmlFor="component-outlined">Added Instructions</InputLabel>
-                <OutlinedInput id="component-outlined" value={name} onChange={handleChange} label="Instructions" />
+                <OutlinedInput id="component-outlined" value={instruct} onChange={handleChange} label="Instructions" name="instruct" />
             </FormControl>
         </Grid>
         </Grid>
